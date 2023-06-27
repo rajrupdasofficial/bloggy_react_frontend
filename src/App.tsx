5af4a2858ec5217715fe6a1f6b63e92ca424f4ee
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-27 12:07:42 +0530

    new signup or authc route component added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Home from './components/Home';
-
+import Authc from './components/authcomponents/Authc';
 const App: React.FC = () => (
 <>
 <Routes>
 <Route path="/" element={ <Home /> }/>
-
+<Route path="/authc" element={ <Authc /> } />
 </Routes>
 </>
 
