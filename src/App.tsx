4dc9bd81e88e329bec333f531a30ec08d1a40094
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-29 18:22:32 +0530

    added profile route
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import Authc from './components/authcomponents/Authc';
+import ProfilePage  from'./components/userprofile/userProfile';
+
 const App: React.FC = () => (
 <>
 <Routes>
 <Route path="/" element={ <Home /> }/>
 <Route path="/authc" element={ <Authc /> } />
+<Route path='/userprofile' element={<ProfilePage /> } />
 </Routes>
 </>
 
