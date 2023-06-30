2b9b899f259c3648cd8dfa45bd7df8306a0c6925
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-30 20:53:59 +0530

    Added logics for logout; added full sanitization after a user loggedout;
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/components/userprofile/userProfile.tsx
+++ src/components/userprofile/userProfile.tsx
@@ -1,8 +1,53 @@
-import react from 'react';
 import './static/userprofile.css';
+import NavBar from '../NavBar';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import React, { useState, useEffect } from 'react';
+import { apiUrl } from '../globals/globalEnv';
+import Cookies from 'universal-cookie';
 
 const ProfilePage: React.FC = () => {
+  const navigate = useNavigate();
+  const [logoutStatus, setLogoutStatus] = useState(false);
+
+    useEffect(() => {
+    if (logoutStatus) {
+      handleLogout();
+    }
+  }, [logoutStatus]);
+
+  const handleLogout = async () => {
+    try {
+      if (apiUrl){
+      const url = `${apiUrl}/users/logout`;
+      const response = await axios.post(url);
+
+      toast.success(response.data.msg);
+
+      const cookies = new Cookies();
+      cookies.remove('_intercom_secure_token');
+      cookies.remove('_intercom_secure_csrf');
+      cookies.remove('_intercom_session_id');
+
+      navigate('/');
+    }else{
+      toast.error("Something went wrong at backend");
+    }
+    }catch (error) {
+      toast.error("Something went wrong at the backend");
+    }
+  };
+
+  const handleClick = () => {
+    setLogoutStatus(true);
+  };
+
+
     return (
+    <div>
+    <NavBar />
+    <ToastContainer />
       <div className="profile-page">
       <div className="profile-header">
       <h1>This is my profile </h1>
@@ -13,7 +58,10 @@ const ProfilePage: React.FC = () => {
       </div>
       <div className="profile-footer">
         {/* Activity feed or additional profile sections */}
+        <h1> This is footer section </h1>
+        <button className="logout-button" type="submit" onClick={handleLogout}>Logout</button>
       </div>
+    </div>
     </div>
     );
 }
