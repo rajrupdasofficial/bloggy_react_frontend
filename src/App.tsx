e73da91aeaf760d2a7748448e913388e46622c06
RajrupDasid <rajrupofficialid@protonmail.com>
2023-06-30 20:45:37 +0530

    profile path added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => (
 <Routes>
 <Route path="/" element={ <Home /> }/>
 <Route path="/authc" element={ <Authc /> } />
-<Route path='/userprofile' element={<ProfilePage /> } />
+<Route path="/userprofile" element={<ProfilePage /> } />
 </Routes>
 </>
 
